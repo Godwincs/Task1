1fd84bd836f2ac487af9f0296b8d474d20a1602e
Godwincs <149038420+Godwincs@users.noreply.github.com>
2026-06-19 10:27:36 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,495 @@
+# aa=11
+# bb=1.1
+# cc="shijin"
+# dd=11/11
+# ee=[1,2,3,4]
+# ff=(1,2,3,4,5)
+# gg={"name":"shijin","age":20}
+# hh=True
+# print(aa)
+# print(bb)
+# print(cc)
+# print(dd)
+# print(ee)
+# print(ff)
+# print(gg)
+# print(hh)
+# print(type(aa))
+# print(type(bb))
+# print(type(cc))
+# print(type(dd))
+# print(type(ee))
+# print(type(ff))
+# print(type(gg))
+# print(type(hh))
+
+
+
+
+# dt_list = [1, 2, 3, 4, 5]
+# print(dt_list)
+
+
+# dt_tuple = (1, 2, 3, 4, 5)
+# print(dt_tuple)
+
+
+# dt_set = {"shijin", "python", "programming"}
+# dt_set.add("shijin1")
+# print(dt_set)
+
+
+# dt_dict = {"name": {"first": "shijin", "last": "CR"}, 
+#            "age": 20}
+# print(dt_dict)
+
+# a={"name":"shijin","age":22,"drp":"comp","cours":["java","python"]}
+# print(a)
+
+# name="shijin"
+# print(name)
+# age=23
+# print(age)
+# height=5.5
+# print(height)
+# fruit=["banana","cerry"]
+# print(fruit)
+# color=("red","blue")
+# print(color)
+
+
+# a=input("enter the number")
+# b=input("enter the numer")
+# sum=int(a)+int(b)
+# print(sum)
+
+# a=input("enter the number:")
+# b=input("enter the number:")
+# add=int(a)+int(b)
+# print(add)
+# sub=int(a)-int(b)
+# print(sub)
+# mul=int(a)*int(b)
+# print(mul)
+# div=int(a)/int(b)
+# print(div)
+# mol=int(a)%int(b)
+# print(mol)
+# flo=int(a)//int(b)
+# print(flo)
+# ex=int(a)**int(b)
+# print(ex)
+
+
+# x=7
+# y=10
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+
+# a=int(input("enter the number :"))
+# if (a>=90):
+#     print("good")
+# elif (a>40):
+#     print("pass")
+# else :
+#     print("fail")
+
+
+# word="programming"
+# for each in word:
+#     print(each)
+
+
+# for i in range(10,0,-1):
+#     print(i)
+
+
+# for i in range(1,10,2):
+#       print(i)
+
+
+# start=1
+# stop=10
+# step=2
+# while(start<stop):
+#     print(start)
+#     start=start+step
+
+
+# for i in range(1,20):
+#     if i%3==0:
+#         continue
+#     print(i)
+
+# user=input("entre the name:")
+# print(user)
+# print("welcome")
+# print(f"welcome,{user}!")
+# print("thank you")
+# for i in range(5):
+#     print("dring water")
+# for i in range(10):
+#     print("drin water")
+# print("bye bye😘😘😘")    
+
+# def cube(num):
+#     return num**3
+# result=cube(5)
+# print(result)
+
+# def average(x,y,z):
+#     return (x+y+z)/3
+# avg=average(10,20,30)
+# print(avg)
+
+
+
+# name=["s","h","i","j","i","n"]
+# print(name[-1::-1])
+# print(name[3])
+
+# list_stu=["ashikha","shelphin","abisha","shijin","godwin"]
+# print(list_stu[1:-1])
+
+
+# email="devil123@gmail.com"
+# print(email)
+# print(email[0:6])
+# print(email[6:14])
+
+
+# name=input("Enter a String:")
+# if name==name[::-1]:
+#     print("It is palindrome")
+# else:
+#     print("It is not a palindrome")
+
+
+# li_value=[23,54,76,43,89]
+# largest_value=max(li_value)
+# print(largest_value)
+
+
+# n = int(input("Enter the number of terms: "))
+# first = 0
+# second = 1
+# print(first)
+# print(second)
+# for i in range(2, n):
+#     next = first + second
+#     print(next)
+#     first = second
+#     second = next
+
+
+for i in range(1,20):
+#     if(i%3==0):
+#         continue
+#     print(i)
+
+
+# username=input("enter your name:")
+# print("username")
+# print("welcome")
+# print(f"welcome",{username})
+# print("thankyou")
+# for i in range(5):
+#     print("drink water")
+# for i in range(10):
+#     print("drink water")
+# print("bye bye")
+
+
+
+# def avg(x,y,z):
+#      result=x+y+z/3
+#      return result
+# result=avg(10,20,30)
+# print(result)
+
+
+# def sum_number(n):
+#     for i in range()
+
+
+# n=[1,2,3,4,5,6,7,8,9]
+# print(min(n))
+# print(max(n))
+# print(sum(n))
+# Start = 1
+
+# list=["shelpyyyyy","bullet","duke","goduwin","intmani","abi"]
+# print(list[-3])
+
+
+# name=["s","h","e","l","p","i","n"]
+# print(name[::-1])
+# print(name[4])
+
+# list=["ashikha","shelphin","shijin","abisha","godwin"]
+# print(list[1:-1])
+#
+
+
+# email="abi123@.com"
+
+# username,domain=email.split("@")
+# print(username)
+# print(domain)
+
+# email=input("enter a mail id:")
+# email_split=email.split("@")
+# username=email_split[0]
+# domain=email_split[1]
+# print(username)
+# print(domain)
+
+# list=["abi","ashi","sheluupyyyyyyy","bullet pandiiii","goduwin"]
+# list.append("duke")
+# print(list)
+# list.remove("abi")
+# print(list)
+# list[1]="ashikha"
+# print(list)
+
+# details={"name":"abisha","department":"testing"}
+# details.update{"city":"mtm"}
+# print(details)
+
+
+
+
+# name=input("Enter a String:")
+# if name==name[::-1]:
+#     print("It is palindrome")
+# else:
+#     print("It is not a palindrome")
+
+
+
+# num = int(input("Enter a number: "))
+# if num > 1:
+#     for i in range(2,num):
+#         if num % i == 0:
+#             print("Not a Prime Number")
+#             break
+#     else:
+#         print("Prime Number")
+# else:
+#     print("Not a Prime Number")
+
+
+
+
+# value=[23,54,76,43,89]
+# large_value=max(value)
+# print(large_value)
+
+# n=int(input("enter a number 0f terms:"))
+# l=0
+# p=1
+# print(l)
+# print(p)
+# for i in range(2, n):
+#     next = l+p
+#     print(next)
+#     l = p
+#     p= next=
+    
+    
+# list=[2,5,3,7,89,0,2,4,6]
+# list.append("duke")
+# print(list)
+# list.remove("abi")
+# print(list)
+
+
+# class student:
+#     def  greet(self):
+#         print("hello")
+#     def student_details(self,name):
+#         self.student_name=name
+#         print(self.student_name)
+# stu=student()
+# stu.greet()
+# stu.student_details("abi")
+
+
+# class Calculator:
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+
+#     def add(self):
+#         return self.a + self.b
+
+#     def subtract(self):
+#         return self.a - self.b
+
+#     def multiply(self):
+#         return self.a * self.b
+
+#     def divide(self):
+#         if self.b == 0:
+#             return "Cannot divide by zero"
+#         return self.a / self.b
+
+
+# Example usage
+# calc = Calculator(10, 5)
+
+# print("Addition:", calc.add())
+# print("Subtraction:", calc.subtract())
+# print("Multiplication:", calc.multiply())
+# print("Division:", calc.divide())
+
+# class calculator:
+#     def add_num(self,a,b):
+#         return a+b
+#     def sub_num(self,a,b):
+#         return a-b
+#     def multiply_num(self,a,b):
+#         return a*b
+#     def divide_num(self,a,b):
+#         return a/b
+
+# sum=calculator()
+# result1=sum.add_num(5,7)
+# print (result1)
+# min=calculator()
+# result2=min.sub_num(9,3)
+# print(result2)
+# into=calculator()
+# result3=into.multiply_num(3,3)
+# print(result3)
+# va=calculator()
+# result4=va.divide_num(8,5)
+# print(result4)
+
+# import random
+
+# # Random number generate (1 to 100)
+# secret_number = random.randint(1, 100)
+
+# print("Welcome to the Number Guessing Game!")
+# print("Guess a number between 1 and 100")
+
+# while True:
+#     guess = int(input("Enter your guess: "))
+
+#     if guess < secret_number:
+#         print("Too low! Try again.")
+    
+#     elif guess > secret_number:
+#         print("Too high! Try again.")
+    
+#     else:
+#         print("🎉 Congratulations! You guessed the correct number!")
+#         break
+
+
+
+# import random
+
+# # Choices list
+# choices = ["rock", "paper", "scissors"]
+
+# print("🎮 Welcome to Rock-Paper-Scissors Game")
+
+# while True:
+#     # User choice
+#     user_choice = input("Enter rock, paper, or scissors: ").lower()
+
+#     if user_choice not in choices:
+#         print("❌ Invalid choice! Please try again.")
+#         continue
+
+#     # Computer choice
+#     computer_choice = random.choice(choices)
+
+    # print("Computer chose:", computer_choice)
+
+    # # Game logic
+    # if user_choice == computer_choice:
+    #     print("🤝 It's a tie!")
+
+    # elif (
+    #     (user_choice == "rock" and computer_choice == "scissors") or
+    #     (user_choice == "paper" and computer_choice == "rock") or
+    #     (user_choice == "scissors" and computer_choice == "paper")
+    # ):
+    #     print("🎉 You win!")
+
+    # else:
+    #     print("😢 Computer wins!")
+
+    # # Play again option
+    # play_again = input("Do you want to play again? (yes/no): ").lower()
+
+    # if play_again != "yes":
+    #     print("👋 Thanks for playing!")
+    #     break
+    
+    
+    
+    
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# operator = input("Enter operator (+, -, *, /): ")
+
+# if operator == "+":
+#     print("Result =", num1 + num2)
+
+# elif operator == "-":
+#     print("Result =", num1 - num2)
+
+# elif operator == "*":
+#     print("Result =", num1 * num2)
+
+# elif operator == "/":
+#     if num2 != 0:
+#         print("Result =", num1 / num2)
+#     else:
+#         print("Cannot divide by zero")
+
+# else:
+#     print("Invalid operator")
+
+
+# n = int(input("Enter a number: "))
+
+# if n > 0:
+#     print("Positive Number")
+# elif n < 0:
+#     print("Negative Number")
+# else:
+#     print("Zero")
+
+# n= int(input("Enter a number: "))
+
+# if n % 2 == 0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+    
+    
+ n= input("Enter a character: ").lower()
+
+ if n in ['a', 'e', 'i', 'o', 'u']:
+    print("Vowel")
+else:
+    print("Consonant")
+    
+    
+    
+n= int(input("Enter a number: "))
+
+if -9 <= n <= 9:
+    print("Single-Digit Number")
+else:
+    print("Multi-Digit Number")
